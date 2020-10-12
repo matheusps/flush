ce28b20bce77038aa511e3b6a3f7a60c9e86a81f
matheusps <cout.matheusps@gmail.com>
2020-10-12 10:45:25 -0300

    feat(system-ui): best export from system and ui bootstrap

--- packages/flush-system/src/ThemeProvider.tsx
+++ packages/flush-system/src/ThemeProvider.tsx
@@ -1,14 +1,16 @@
-import React, { ReactNode, memo } from 'react'
+import React, { ReactNode, memo, useMemo } from 'react'
 import { ThemeProvider as ThemeUIProvider } from '@theme-ui/core'
 import 'focus-visible/dist/focus-visible'
+import merge from 'deepmerge'
 
-import { theme } from './theme'
+import { theme as defaultTheme } from './theme'
 
 interface Props {
   children: ReactNode
+  theme?: any
 }
 
-export function ThemeProvider({ children }: Props) {
+export function ThemeProvider({ children, theme: custonTheme = {} }: Props) {
+  const theme = useMemo(() => merge(defaultTheme, custonTheme), [])
   return <ThemeUIProvider theme={theme}>{children}</ThemeUIProvider>
 }
-
